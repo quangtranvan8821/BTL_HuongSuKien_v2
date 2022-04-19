2ddf71aaf8e09731c380592bfadc4870db89f628
nobita31a <nobita31a@gmail.com>
2022-04-20 00:06:13 +0700

    them ConnectDatabase

---
+++ BTL_HuongSuKien_v2/ConnectDatabase/ConnectDatabase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL_HuongSuKien_v2.ConnectDatabase
+{
+    class ConnectDatabase
+    {
+        //connect to db
+        public SqlConnection getConnect()
+        {
+            //connect string here
+            String strConn = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+            return new SqlConnection(strConn);
+        }
+        public DataTable getTable(String sql)
+        {
+            DataTable dt = new DataTable();
+            SqlConnection conn = getConnect();
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+        public void ExcuteNonQuery(String sql)
+        {
+            SqlConnection conn = getConnect();
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            cmd.Clone();
+            conn.Close();
+        }
+        public String ExcuteScalar(String sql)
+        {
+            SqlConnection conn = getConnect();
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            String kq = cmd.ExecuteScalar().ToString();
+            conn.Close();
+            return kq;
+        }
+    }
+}
